8e4af7b16291f028cba99e644f2fa7744a802ee8
lutrapp <lutrapp@gmail.com>
2021-04-24 17:06:56 -0300

    gerando as divs a partir do javascript

--- jogo.js
+++ jogo.js
@@ -1,16 +1,32 @@
 var pontos = document.getElementById('pontos');
 var bomba = document.getElementById('bombas');
-
-function jogar() {
-    pontos.innerHTML = 0;
-    bomba.innerHTML = 0;
-
-}
 var somaPontos = 0;
 var somaBombas = 0;
+// este é o array de todas as divs do campo
+var campoDiv = Array();
+
+
+function jogar() {
+    somaPontos = 0;
+    somaBombas = 0;
+    pontos.innerHTML = 0;
+    bomba.innerHTML = 0;
+    // para limpar os backgrounds com figuras
+    campoDiv.forEach(element => {
+        element.style.backgroundImage = "";
+        element.firstElementChild.value = novoValor();
+
+    });
+}
+// para gerar um número aleatório 0 ou 1
+function novoValor() {
+    x = Math.round(Math.random() * 1);
+    return x;
+}
+
+
 // acessa os inputs que tem ou não o elemento "bomba"
 function verificaCampo() {
-
     divVerifica = this.firstElementChild.value;
     if (divVerifica == 0) {
         this.style.backgroundImage = "url(img/grama.jpg)";
@@ -20,20 +36,17 @@ function verificaCampo() {
         this.style.backgroundImage = "url(img/bomba.jpg)";
         somaBombas += 1;
     }
-
     pontos.innerHTML = somaPontos;
     bombas.innerHTML = somaBombas;
 }
 
 // para conseguir acessar todos as divs
 for (i = 0; i < 100; i++) {
-    campo = document.querySelector('section').getElementsByTagName('div')[i];
+    var section = document.querySelector('section');
+    section.insertAdjacentHTML('beforeend', '<div><input type="hidden" value="' + novoValor() + '" ></div>');
+
+    let campo = section.getElementsByTagName('div')[i];
+    campoDiv[i] = campo;
     campo.addEventListener('click', verificaCampo);
 }
 // console.log(campo);
-
-function embaralha() {
-
-}
-
-console.log(embaralha())
